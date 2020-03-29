f5140d57919a976c1aa75009e346aa681c254169
Maxim Shakurov <m.shakurov99@mail.ru>
2020-03-30 00:13:56 +0400

    Добавлено прямолинейное движение самолетов и корабли с портами

--- Module1/TestModule.cs
+++ Module1/TestModule.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Module1
 {
@@ -18,6 +19,38 @@ namespace Module1
         Airport airport3;
         Airport airport4;
         Airport airport5;
+
+        Airplane airplane1;
+        Airplane airplane2;
+        Airplane airplane3;
+        Airplane airplane4;
+        Airplane airplane5;
+
+        Port port1;
+        Port port2;
+        Port port3;
+        Port port4;
+        Port port5;
+
+        Ship ship1;
+        Ship ship2;
+        Ship ship3;
+        Ship ship4;
+        Ship ship5;
+
+
+        Coordinate CoordinateDep1;
+        Coordinate CoordinateDep2;
+        Coordinate CoordinateDep3;
+        Coordinate CoordinateDep4;
+        Coordinate CoordinateDep5;
+        Coordinate CoordinateDepShip1;
+        Coordinate CoordinateDepShip2;
+        Coordinate CoordinateDepShip3;
+        Coordinate CoordinateDepShip4;
+        Coordinate CoordinateDepShip5;
+        // Airplane[] airplanes;
+        //Coordinate[] coordinateDepAir;
 
         protected override void Initialize()
         {
@@ -33,24 +66,153 @@ namespace Module1
             airport4 = new Airport(airport4Coordinate);
             airport5 = new Airport(airport5Coordinate);
 
+            port1 = new Port(new Coordinate(-3991847, -596820));
+            port2 = new Port(new Coordinate(-4520180, -2299226));
+            port3 = new Port(new Coordinate(1193641, -381574));
+            port4 = new Port(new Coordinate(2074195, -3982063));
+            port5 = new Port(new Coordinate(12914800, -4138606));
+
+
+            MapObjects.Add(port1);
+            MapObjects.Add(port2);
+            MapObjects.Add(port3);
+            MapObjects.Add(port4);
+            MapObjects.Add(port5);
             MapObjects.Add(airport1);
             MapObjects.Add(airport2);
             MapObjects.Add(airport3);
             MapObjects.Add(airport4);
             MapObjects.Add(airport5);
+
+
+
+           // airplanes = CreateAirplanes();
+           // for (int i = 0; i < 20; i++)
+           // {
+            //    coordinateDepAir[i] = airplanes[i].coordinateDepartureAirplane;
+            //}
+            airplane1 = CreateAirplane(MathExtensions.LatLonToSpherMerc(-34.831747, -56.020034));
+            airplane2 = CreateAirplane(new Coordinate(4942686, 6234338));
+            airplane3 = CreateAirplane(new Coordinate(-8579292, 4700571));
+            airplane4 = CreateAirplane(new Coordinate(15547903, 4259071));
+            airplane5 = CreateAirplane(new Coordinate(5241746, -2178150));
+
+            ship1 = CreateShip(new Coordinate(-3991847, -596820));
+            ship2 = CreateShip(new Coordinate(-4520180, -2299226));
+            ship3 = CreateShip(new Coordinate(1193641, -381574));
+            ship4 = CreateShip(new Coordinate(2074195, -3982063));
+            ship5 = CreateShip(new Coordinate(12914800, -4138606));
         }
 
         public Airplane CreateAirplane(Coordinate coordinateAirport)
         {
             Airplane airplane;
+            
             Random rnd = new Random();
             Coordinate coordinateDeparture = coordinateAirport;
-            double speed = rnd.Next(10000, 100000);
-            Coordinate coordinateArrival = new Coordinate(4942686, 6234338);
+            Coordinate coordinateArrival = new Coordinate(0,0);
+            double speed = rnd.Next(1000, 2000);
+            do
+            {
+                int rndForCoordinate = rnd.Next(1, 5);
+                switch (rndForCoordinate)
+                {
+                    case 1:
+                        coordinateArrival = MathExtensions.LatLonToSpherMerc(-34.831747, -56.020034);
+                        break;
+                    case 2:
+                        coordinateArrival = new Coordinate(4942686, 6234338);
+                        break;
+                    case 3:
+                        coordinateArrival = new Coordinate(-8579292, 4700571);
+                        break;
+                    case 4:
+                        coordinateArrival = new Coordinate(15547903, 4259071);
+                        break;
+                    case 5:
+                        coordinateArrival = new Coordinate(5241746, -2178150);
+                        break;
+                }
+            } while (coordinateDeparture.Equals2D(coordinateArrival));
             airplane = new Airplane(coordinateDeparture, speed, coordinateArrival);
+
+            var lineCoordinates = new Coordinate[] { airplane.coordinateDepartureAirplane, airplane.coordinateArrivalAirplane };
+            //airplane.line = new LineString(lineCoordinates);
+           // MapObjects.Add(airplane.line);
             MapObjects.Add(airplane);
             return airplane;
         }
+
+
+        public Ship CreateShip(Coordinate coordinateAirport)
+        {
+            Ship ship;
+
+            Random rnd = new Random();
+            Coordinate coordinateIntermediate = new Coordinate(0, 0);
+            Coordinate coordinateDeparture = coordinateAirport;
+            Coordinate coordinateArrival = new Coordinate(0, 0);
+            double speed = rnd.Next(1000, 2000);
+            do
+            {
+                int rndForCoordinate = rnd.Next(1, 5);
+                switch (rndForCoordinate)
+                {
+                    case 1:
+                        coordinateArrival = new Coordinate(-3991847, -596820);
+                        //coordinateIntermediate = new Coordinate()
+                        break;
+                    case 2:
+                        coordinateArrival = new Coordinate(-4520180, -2299226);
+                        break;
+                    case 3:
+                        coordinateArrival = new Coordinate(1193641, -381574);
+                        break;
+                    case 4:
+                        coordinateArrival = new Coordinate(2074195, -3982063);
+                        break;
+                    case 5:
+                        coordinateArrival = new Coordinate(12914800, -4138606);
+                        break;
+                }
+            } while (coordinateDeparture.Equals2D(coordinateArrival));
+            ship = new Ship(coordinateDeparture, speed, coordinateArrival);
+            MapObjects.Add(ship);
+            return ship;
+        }
+
+        public Coordinate RandomCoordinateArrival(Coordinate coordinateDeparture)
+        {
+            Random rnd = new Random();
+            Coordinate coordinateArrival = new Coordinate(0, 0);
+            int rndForCoordinate;
+            do
+            {
+                rndForCoordinate = rnd.Next(1, 5);
+                switch (rndForCoordinate)
+                {
+                    case 1:
+                        coordinateArrival = MathExtensions.LatLonToSpherMerc(-34.831747, -56.020034);
+                        break;
+                    case 2:
+                        coordinateArrival = new Coordinate(4942686, 6234338);
+                        break;
+                    case 3:
+                        coordinateArrival = new Coordinate(-8579292, 4700571);
+                        break;
+                    case 4:
+                        coordinateArrival = new Coordinate(15547903, 4259071);
+                        break;
+                    case 5:
+                        coordinateArrival = new Coordinate(5241746, -2178150);
+                        break;
+                }
+            } while (coordinateDeparture.Equals2D(coordinateArrival));
+            return coordinateArrival;
+        }
+
+
+        
         /// <summary>
         /// Вызывается постоянно, здесь можно реализовывать логику перемещений и всего остального, требующего времени.
         /// </summary>
@@ -58,11 +220,116 @@ namespace Module1
         public override void Update(long elapsedMilliseconds)
         {
 
-            CreateAirplane(MathExtensions.LatLonToSpherMerc(-34.831747, -56.020034)).MoveUpRight();
-            CreateAirplane(new Coordinate(4942686, 6234338)).MoveUpRight();
-            CreateAirplane(new Coordinate(-8579292, 4700571)).MoveUpRight();
-            CreateAirplane(new Coordinate(15547903, 4259071)).MoveUpRight();
-            CreateAirplane(new Coordinate(5241746, -2178150)).MoveUpRight();
+
+
+
+            //for (int i = 0; i < 20; i++)
+            //{
+
+            //   airplanes[i].straight_line_equation(coordinateDepAir[i], airplanes[i].coordinateArrivalAirplane);
+            // }
+            if (airplane1.reached && airplane2.reached && airplane3.reached && airplane4.reached && airplane5.reached)
+            {
+                CoordinateDep1 = airplane1.coordinateDepartureAirplane.Copy();
+                CoordinateDep2 = airplane2.coordinateDepartureAirplane.Copy();
+                CoordinateDep3 = airplane3.coordinateDepartureAirplane.Copy();
+                CoordinateDep4 = airplane4.coordinateDepartureAirplane.Copy();
+                CoordinateDep5 = airplane5.coordinateDepartureAirplane.Copy();
+                airplane1.reached = false;
+                airplane2.reached = false;
+                airplane3.reached = false;
+                airplane4.reached = false;
+                airplane5.reached = false;
+
+                CoordinateDepShip1 = ship1.coordinateDepartureShip.Copy();
+                CoordinateDepShip2 = ship2.coordinateDepartureShip.Copy();
+                CoordinateDepShip3 = ship3.coordinateDepartureShip.Copy();
+                CoordinateDepShip4 = ship4.coordinateDepartureShip.Copy();
+                CoordinateDepShip5 = ship5.coordinateDepartureShip.Copy();
+                ship1.reached = false;
+                ship2.reached = false;
+                ship3.reached = false;
+                ship4.reached = false;
+                ship5.reached = false;
+            }
+
+            airplane1.straight_line_equation(CoordinateDep1, airplane1.coordinateArrivalAirplane);
+            airplane2.straight_line_equation(CoordinateDep2, airplane2.coordinateArrivalAirplane);
+            airplane3.straight_line_equation(CoordinateDep3, airplane3.coordinateArrivalAirplane);
+            airplane4.straight_line_equation(CoordinateDep4, airplane4.coordinateArrivalAirplane);
+            airplane5.straight_line_equation(CoordinateDep5, airplane5.coordinateArrivalAirplane);
+
+            ship1.straight_line_equation(CoordinateDepShip1, ship1.coordinateArrivalShip);
+            ship2.straight_line_equation(CoordinateDepShip2, ship2.coordinateArrivalShip);
+            ship3.straight_line_equation(CoordinateDepShip3, ship3.coordinateArrivalShip);
+            ship4.straight_line_equation(CoordinateDepShip4, ship4.coordinateArrivalShip);
+            ship5.straight_line_equation(CoordinateDepShip5, ship5.coordinateArrivalShip);
+
+            if (airplane1.reached)
+            {
+                airplane1.reached = false;
+                CoordinateDep1 = airplane1.coordinateArrivalAirplane.Copy();
+                //airplane1 = CreateAirplane(CoordinateDep1);
+                //MapObjects.Get<LineString>().
+                airplane1.coordinateArrivalAirplane = RandomCoordinateArrival(airplane1.coordinateArrivalAirplane);
+                //airplane1.line.Coordinates
+                //var lineCoordinates = new Coordinate[] { airplane1.coordinateDepartureAirplane, airplane1.coordinateArrivalAirplane };
+                
+            }
+            if (airplane2.reached)
+            {
+                airplane2.reached = false;
+                CoordinateDep2 = airplane2.coordinateArrivalAirplane.Copy();
+                //airplane2 = CreateAirplane(CoordinateDep2);
+                
+
+
+                airplane2.coordinateArrivalAirplane = RandomCoordinateArrival(airplane2.coordinateArrivalAirplane);
+                //airplane1.line.Coordinates
+                //var lineCoordinates = new Coordinate[] { airplane2.coordinateDepartureAirplane, airplane2.coordinateArrivalAirplane };
+                //airplane2.line = new LineString(lineCoordinates);
+
+            }
+            if (airplane3.reached)
+            {
+                airplane3.reached = false;
+                CoordinateDep3 = airplane3.coordinateArrivalAirplane.Copy();
+                //airplane3 = CreateAirplane(CoordinateDep3);
+               
+
+                airplane3.coordinateArrivalAirplane = RandomCoordinateArrival(airplane3.coordinateArrivalAirplane);
+                //airplane1.line.Coordinates
+                //var lineCoordinates = new Coordinate[] { airplane3.coordinateDepartureAirplane, airplane3.coordinateArrivalAirplane };
+                //airplane3.line = new LineString(lineCoordinates);
+
+            }
+            if (airplane4.reached)
+            {
+                airplane4.reached = false;
+                CoordinateDep4 = airplane4.coordinateArrivalAirplane.Copy();
+                //airplane4 = CreateAirplane(CoordinateDep4);
+               
+
+                airplane4.coordinateArrivalAirplane = RandomCoordinateArrival(airplane4.coordinateArrivalAirplane);
+                //airplane1.line.Coordinates
+                //var lineCoordinates = new Coordinate[] { airplane4.coordinateDepartureAirplane, airplane4.coordinateArrivalAirplane };
+                //airplane4.line = new LineString(lineCoordinates);
+
+            }
+            if (airplane5.reached)
+            {
+                airplane5.reached = false;
+                CoordinateDep5 = airplane5.coordinateArrivalAirplane.Copy();
+                //airplane5 = CreateAirplane(CoordinateDep5);
+                
+
+                airplane5.coordinateArrivalAirplane = RandomCoordinateArrival(airplane5.coordinateArrivalAirplane);
+                //airplane1.line.Coordinates
+                //var lineCoordinates = new Coordinate[] { airplane5.coordinateDepartureAirplane, airplane5.coordinateArrivalAirplane };
+                //airplane5.line = new LineString(lineCoordinates);
+
+            }
+
         }
     }
 
@@ -89,12 +356,17 @@ namespace Module1
         ")] // Переопределим стиль всех объектов данного класса, сделав самолет фиолетовым, используя атрибут CustomStyle.
     public class Airplane : Point // Унаследуем данный данный класс от стандартной точки.
     {
-
+        public bool reached = true; //показыает достиг ли самолет точки назначения
         public double Speed { get; }
+        public Coordinate coordinateArrivalAirplane;
+        public Coordinate coordinateDepartureAirplane;
+       // public LineString line;
 
 
         public Airplane(Coordinate coordinate, double speed, Coordinate coordinateArrival) : base(coordinate)
         {
+            coordinateArrivalAirplane = coordinateArrival;
+            coordinateDepartureAirplane = coordinate;
             Speed = speed;
         }
 
@@ -106,6 +378,31 @@ namespace Module1
             X += Speed;
             Y += Speed;
         }
+
+        public void straight_line_equation(Coordinate coordinate, Coordinate coordinateArrival)
+        {
+            if (!reached)
+            {
+                if (X > coordinateArrival.X)
+                {
+                    X -= Speed;
+                }
+                else
+                {
+                    X += Speed;
+                }
+                Y = (-(coordinate.Y - coordinateArrival.Y) * X - (coordinate.X * coordinateArrival.Y - coordinateArrival.X * coordinate.Y)) / (coordinateArrival.X - coordinate.X);  
+            }
+
+            if (Y < coordinateArrival.Y + 2000 && X < coordinateArrival.X + 2000 && Y < coordinateArrival.Y + 2000 && X > coordinateArrival.X - 2000 && Y > coordinateArrival.Y - 2000 && X > coordinateArrival.X - 2000 && Y > coordinateArrival.Y - 2000 && X < coordinateArrival.X + 2000)
+            {
+                reached = true;
+            }
+        }
+
+        
+
+
     }
     #endregion
 
@@ -134,4 +431,86 @@ namespace Module1
 
 
     }
+
+    [CustomStyle(
+      @"new ol.style.Style({
+            image: new ol.style.Circle({
+                opacity: 3.0,
+                scale: 3.0,
+                radius: 6,
+                fill: new ol.style.Fill({
+                    color: 'Blue'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 0, 0, 0.4)',
+                    width: 1
+                }),
+            })
+        });
+        ")]
+    class Port : Point
+    {
+        public Port(Coordinate coordinate) : base(coordinate)
+        {
+        }
+    }
+
+    [CustomStyle(
+      @"new ol.style.Style({
+            image: new ol.style.Circle({
+                opacity: 1.0,
+                scale: 1.0,
+                radius: 3,
+                fill: new ol.style.Fill({
+                    color: 'Blue'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 0, 0, 0.4)',
+                    width: 1
+                }),
+            })
+        });
+        ")]
+
+    public class Ship : Point
+    {
+        public bool reached = true; //показыает достиг ли самолет точки назначения
+        public bool reacheIntermediate = false;
+        public double Speed { get; }
+        public Coordinate coordinateArrivalShip;
+        public Coordinate coordinateIntermediate;
+        public Coordinate coordinateDepartureShip;
+        // public LineString line;
+
+
+        public Ship(Coordinate coordinate, double speed, Coordinate coordinateArrival) : base(coordinate)
+        {
+            coordinateArrivalShip = coordinateArrival;
+            coordinateDepartureShip = coordinate;
+            Speed = speed;
+        }
+
+        public void straight_line_equation(Coordinate coordinate, Coordinate coordinateArrival)
+        {
+            if (!reached)
+            {
+                if (X > coordinateArrival.X)
+                {
+                    X -= Speed;
+                }
+                else
+                {
+                    X += Speed;
+                }
+                Y = (-(coordinate.Y - coordinateArrival.Y) * X - (coordinate.X * coordinateArrival.Y - coordinateArrival.X * coordinate.Y)) / (coordinateArrival.X - coordinate.X);
+            }
+
+            if (Y < coordinateArrival.Y + 2000 && X < coordinateArrival.X + 2000 && Y < coordinateArrival.Y + 2000 && X > coordinateArrival.X - 2000 && Y > coordinateArrival.Y - 2000 && X > coordinateArrival.X - 2000 && Y > coordinateArrival.Y - 2000 && X < coordinateArrival.X + 2000)
+            {
+                reached = true;
+            }
+        }
+    }
+
+
 }
